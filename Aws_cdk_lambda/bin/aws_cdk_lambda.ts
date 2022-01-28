a3e5a7d7011dc467d518378104d011ccf8c19adb
Zia Ur Rehman <ziaalikhan052@gmail.com>
2022-01-28 21:09:09 +0500

    update

--- Aws_cdk_lambda/bin/aws_cdk_lambda.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AwsCdkLambdaStack } from '../lib/aws_cdk_lambda-stack';
-
-const app = new cdk.App();
-new AwsCdkLambdaStack(app, 'AwsCdkLambdaStack');
